bfd70d6dcb7fc1cc9141c5fdabb7b30ec1e91a99
polishcode.eu <polishcode@gmail.com>
2022-06-23 17:25:51 +0200

    OT-376 - resolve PR comments

--- _devel/ci/build-in-container-clang.sh
+++ _devel/ci/build-in-container-clang.sh
@@ -4,4 +4,4 @@ docker run \
     --mount type=bind,src=/mnt/f/repo-matterfi/opentxs,dst=/home/src,readonly \
     --mount type=bind,src=/home/pgawron/opentxs/output,dst=/home/output \
     -i --entrypoint /usr/bin/build-opentxs-clang.sh \
-    polishcode/matterfi-ci-fedora:35-1 nopch
+    polishcode/matterfi-ci-fedora:35-1 test01
--- _devel/ci/build-in-container-gcc.sh
+++ _devel/ci/build-in-container-gcc.sh
@@ -4,4 +4,4 @@ docker run \
     --mount type=bind,src=/mnt/f/repo-matterfi/opentxs,dst=/home/src,readonly \
     --mount type=bind,src=/home/pgawron/opentxs/output,dst=/home/output \
     -i --entrypoint /usr/bin/build-opentxs-gcc.sh \
-    polishcode/matterfi-ci-fedora:35-1 full
+    polishcode/matterfi-ci-fedora:35-1 test01
