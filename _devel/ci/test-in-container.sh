d4a03437ac07f551c5cd815e2396da58c31a908a
polishcode.eu <polishcode@gmail.com>
2022-07-18 16:55:46 +0200

    OT-377 added support for clang sanitizer build
    - introduced build modes: standard, ccov, sanitizer
    - added new ci docker image (35-3)
    - test script runs ctest verbose
    - clang supports standard + sanitizer
    - gcc supports standard + ccov
    - new client scripts

--- _devel/ci/test-in-container.sh
+++ _devel/ci/test-in-container.sh
@@ -4,4 +4,4 @@ docker run \
     --mount type=bind,src=/mnt/f/repo-matterfi/opentxs,dst=/home/src,readonly \
     --mount type=bind,src=/home/pgawron/opentxs/output,dst=/home/output \
     -i --entrypoint /usr/bin/test-opentxs.sh \
-    polishcode/matterfi-ci-fedora:35-2 "-R ottest-blockchain-address"
+    polishcode/matterfi-ci-fedora:35-3 "-V -R ottest-blockchain-address"
